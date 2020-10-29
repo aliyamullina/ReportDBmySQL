6d5f080cf7d1d11f7e0d92feba75a1e8f39864d4
Aliya Mullina <aliya.mullina@ya.ru>
2020-10-29 23:13:16 +0300

    Add table Addresses

--- DB.cs
+++ DB.cs
@@ -11,6 +11,7 @@ namespace ReportDBmySQL
             "Password = root; " +
             "database = reportdbmysqlhomes"
             );
+
 CREATE DATABASE productsdb;
 USE productsdb;
 CREATE TABLE Cities
@@ -25,12 +26,20 @@ VALUES ('Нурлат'),
 VALUES ('Чистополь'),
 VALUES ('Высокая гора');
 
-1 таблица
-IdГород 1, 1
-Улица Большая, Подлужная
-Дом 50, 40
-IdУлица 1, 2
-IsДом 1, 2
+CREATE DATABASE productsdb;
+USE productsdb;
+CREATE TABLE Addresses
+(
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Address VARCHAR(30) NOT NULL,
+    Home VARCHAR(10),
+    Id_city,
+);
+
+INSERT Cities(Id, Address, Home, Id_city) 
+VALUES ('Большая', '80', 1),
+VALUES ('Подлужная', '40', 1);
+
 
 2 таблица Реестр
 IdУлица
