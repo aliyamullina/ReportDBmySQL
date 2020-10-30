78674f51dda31031d7291d42107d211667ac26ec
Aliya Mullina <aliya.mullina@ya.ru>
2020-10-30 09:34:30 +0300

    Создается базовая коллекция Адреса - Город, Улица, Дом

---
+++ AddressInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace ReportDBmySQL
+{
+    class AddressInfo
+    {
+
+        public AddressInfo(string city, string street, string home)
+        {
+            this.City = city;
+            this.Street = street;
+            this.Home = home;
+        }
+
+        public string City { get; private set; }
+        public string Street { get; private set; }
+        public string Home { get; private set; }
+    }
+}
