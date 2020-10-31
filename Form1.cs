36b749626e4f961a96cd333cc135c80c71e3b9d3
Aliya Mullina <aliya.mullina@ya.ru>
2020-10-31 16:48:55 +0300

    Чистка кода от пробелов -ОК

--- Form1.cs
+++ Form1.cs
@@ -24,35 +24,22 @@ namespace ReportDBmySQL
         private static List<AddressInfo> getFolderAddressInfo()
         {
             List<AddressInfo> folderAdress = new List<AddressInfo>();
-
             FolderBrowserDialog folderDlg = new FolderBrowserDialog { ShowNewFolderButton = true };
-
             DialogResult dialog = folderDlg.ShowDialog();
-
             if (dialog == DialogResult.OK)
             {
                 string PathToFolder = folderDlg.SelectedPath;
                 _ = folderDlg.RootFolder;
-
                 string[] allfolders = Directory.GetDirectories(PathToFolder);
-
                 string city = "1";
-                
                 foreach (var path in allfolders)
                 {
-                    // "Адоратского 27"
                     var pathTrim = path.Substring(path.LastIndexOf("\\")).Replace("\\", string.Empty);
-
-                    // "Адоратского"
                     var street = pathTrim.Substring(0, pathTrim.IndexOf(" "));
-
-                    // " 27А"
                     var home = pathTrim.Substring(pathTrim.LastIndexOf(" ")).Replace(" ", string.Empty);
-
                     folderAdress.Add(new AddressInfo(city, street, home));
                 }
             }
-            Console.WriteLine();
             return folderAdress;
         }
         // Из папки в mysql
