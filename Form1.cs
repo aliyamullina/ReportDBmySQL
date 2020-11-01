c9b4388c97d5985cac3bf35e358b72db52777213
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-01 15:33:31 +0300

    Подсказки со  stackoverflow

--- Form1.cs
+++ Form1.cs
@@ -84,7 +84,33 @@ namespace ReportDBmySQL
             MySqlCommand command = new MySqlCommand("");*/
         }
 
-
+private List<string> CategoriesPicker = new List<string>();
+    //add all the items to the list
+    private void SavedItemsButton_Clicked(object sender, System.EventArgs e)
+    {
+        string sqlstring = "server=; port= ; user id =;Password= ;Database=test;";
+        using (MySqlConnection conn = new MySqlConnection(sqlstring))
+        {
+            string Query = "INSERT INTO test.maintable (Categories)values(@Category);";
+            using (MySqlCommand cmd = new MySqlCommand(Query, conn))
+            {
+                cmd.Parameters.Add("@Category", MySqlDbType.VarChar);
+                try
+                {
+                    conn.Open();
+                }
+                catch (MySqlException ex)
+                {
+                    throw ex;
+                }
+                foreach (String item in CategoriesPicker)
+                {
+                    cmd.Parameters["@Category"].Value = item;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
         // Из папки в mysql
         // Из mysql в массив
         // Mассив передать в макет word с адресом в имени
