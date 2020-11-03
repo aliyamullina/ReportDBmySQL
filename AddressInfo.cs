fb9252dfa6cad9b22c60a3697b519e10ceb28548
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-03 12:07:25 +0300

    Элементы от CatalogInfo Catalog_id - ОК

--- AddressInfo.cs
+++ AddressInfo.cs
@@ -2,15 +2,17 @@
 {
     class AddressInfo
     {
-        public AddressInfo(string street, string home, string city_id)
+        public AddressInfo(string street, string home, string city_id, string catalog_id)
         {
             this.Street = street;
             this.Home = home;
             this.City_id = city_id;
+            this.Catalog_id = catalog_id;
         }
 
         public string Street { get; private set; }
         public string Home { get; private set; }
         public string City_id { get; private set; }
+        public string Catalog_id { get; private set; }
     }
 }
