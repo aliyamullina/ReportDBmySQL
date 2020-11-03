91d91e0a54009ea868518eebb90190f62fae02c6
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-03 09:37:11 +0300

    Класс для хранения путей

---
+++ FileInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportDBmySQL
+{
+    class FileInfo
+    {
+        public FileInfo(string Path)
+        {
+            this.Path = Path;
+        }
+        public string Path { get; private set; }
+    }
+}
