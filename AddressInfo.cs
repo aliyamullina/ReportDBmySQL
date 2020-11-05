5959bd1bf5649b753c39ef9c7f838875bd85cbe8
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-05 08:55:45 +0300

    get;  set; вместо private set; - ОК

--- AddressInfo.cs
+++ AddressInfo.cs
@@ -5,6 +5,10 @@ namespace ReportDBmySQL
 {
     public class AddressInfo
     {
+        public AddressInfo()
+        {
+        }
+
         public AddressInfo(string street, string home, int city_id, int catalog_id)
         {
             this.Street = street;
@@ -12,11 +16,10 @@ namespace ReportDBmySQL
             this.City_id = city_id;
             this.Catalog_id = catalog_id;
         }
-
-        public string Street { get; private set; }
-        public string Home { get; private set; }
-        public int City_id { get; private set; }
-        public int Catalog_id { get; private set; }
+        public string Street { get;  set; }
+        public string Home { get;  set; }
+        public int City_id { get;  set; }
+        public int Catalog_id { get;  set; }
     }
     public partial class DB
     {
@@ -61,6 +64,40 @@ namespace ReportDBmySQL
                 }
                 connection.Close();
             }
+        }
+
+        /// <summary>
+        /// Извлечение из таблицы Catalogs в List
+        /// </summary>
+        public List<AddressInfo> GetAddressList()
+        {
+            List<AddressInfo> addressSelect = new List<AddressInfo>();
+
+            using (MySqlCommand command = new MySqlCommand(@"SELECT * FROM addresses", connection))
+            {
+                connection.Open();
+
+                using (MySqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        AddressInfo address = new AddressInfo();
+
+                        address.Street = dataReader["Street"].ToString();
+                        address.Home = dataReader["Home"].ToString();
+                        address.City_id = (int)dataReader["City_id"];
+                        address.Catalog_id = (int)dataReader["Catalog_id"];
+
+                        addressSelect.Add(street);
+                        addressSelect.Add(home);
+                        addressSelect.Add(city_id);
+                        addressSelect.Add(catalog_id);
+                    }
+                    dataReader.Close();
+                }
+                connection.Close();
+            }
+            return addressSelect;
         }
     }
 }
