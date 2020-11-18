8a1e09cb6173337aafc4640eb9bf214145c4c8bb
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-18 09:45:05 +0300

    GetExcelTableRead, try catch, return List RegistryInfo - OK

--- Classes/ExcelUtility.cs
+++ Classes/ExcelUtility.cs
@@ -7,34 +7,42 @@ namespace ReportDBmySQL
 {
     public class ExcelUtility
     {
-        public static void GetExcelTableRead(string northwinddataXlsx, out List<RegistryInfo> registersList)
+        public static List<RegistryInfo> GetExcelTableRead(string northwinddataXlsx, out List<RegistryInfo> registersList)
         {
             registersList = new List<RegistryInfo>();
 
-            var wb = new XLWorkbook(northwinddataXlsx);
-            var ws = wb.Worksheet(1);
-
-            var firstRowUsed = ws.FirstRowUsed();
-            var categoryRow = firstRowUsed.RowUsed();
-
-            categoryRow = categoryRow.RowBelow();
-
-            const int apartmentRow = 1;
-            const int modelRow = 2;
-            const int serialRow = 3;
-
-            while (!categoryRow.Cell(1).IsEmpty())
+            try
             {
-                string apartment = categoryRow.Cell(apartmentRow).GetString();
-                string model = categoryRow.Cell(modelRow).GetString();
-                string serial = categoryRow.Cell(serialRow).GetString();
+                var wb = new XLWorkbook(northwinddataXlsx);
+                var ws = wb.Worksheet(1);
 
-                registersList.Add(new RegistryInfo(apartment, model, serial));
+                var firstRowUsed = ws.FirstRowUsed();
+                var categoryRow = firstRowUsed.RowUsed();
 
                 categoryRow = categoryRow.RowBelow();
-            }
 
-            Console.WriteLine();
+                const int apartmentRow = 1;
+                const int modelRow = 2;
+                const int serialRow = 3;
+
+                while (!categoryRow.Cell(1).IsEmpty())
+                {
+                    string apartment = categoryRow.Cell(apartmentRow).GetString();
+                    string model = categoryRow.Cell(modelRow).GetString();
+                    string serial = categoryRow.Cell(serialRow).GetString();
+
+                    registersList.Add(new RegistryInfo(apartment, model, serial));
+
+                    categoryRow = categoryRow.RowBelow();
+                }
+
+                Console.WriteLine();
+                return registersList;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
