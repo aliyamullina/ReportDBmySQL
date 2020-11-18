5afb18c34991903733fef5040e42805352b70fce
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-18 10:37:36 +0300

    ClosedXML, первые 3 столбца со сдвигом вниз на 6 - ОК

--- Classes/ExcelUtility.cs
+++ Classes/ExcelUtility.cs
@@ -16,27 +16,18 @@ namespace ReportDBmySQL
                 using (XLWorkbook wb = new XLWorkbook(northwinddataXlsx)) { 
                     var ws = wb.Worksheet(1);
 
-                    var firstRowUsed = ws.FirstRowUsed();
-                    var categoryRow = firstRowUsed.RowUsed();
+                    var rows = ws.RangeUsed().RowsUsed().Skip(6);
 
-                    categoryRow = categoryRow.RowBelow();
-
-                    const int apartmentRow = 1;
-                    const int modelRow = 2;
-                    const int serialRow = 3;
-
-                    while (!categoryRow.Cell(1).IsEmpty())
+                    foreach (var row in rows)
                     {
-                        string apartment = categoryRow.Cell(apartmentRow).GetString();
-                        string model = categoryRow.Cell(modelRow).GetString();
-                        string serial = categoryRow.Cell(serialRow).GetString();
+
+                        string apartment = row.Cell(1).Value.ToString();
+                        string model = row.Cell(2).Value.ToString();
+                        string serial = row.Cell(3).Value.ToString();
 
                         registersList.Add(new RegistryInfo(apartment, model, serial));
-
-                        categoryRow = categoryRow.RowBelow();
                     }
                 }
-                Console.WriteLine();
                 return registersList;
             }
             catch
