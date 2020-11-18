121321a7a25f4aa86c6800384cee7e9604bc39ae
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-18 09:47:51 +0300

    GetExcelTableRead, using - OK

--- Classes/ExcelUtility.cs
+++ Classes/ExcelUtility.cs
@@ -13,29 +13,29 @@ namespace ReportDBmySQL
 
             try
             {
-                var wb = new XLWorkbook(northwinddataXlsx);
-                var ws = wb.Worksheet(1);
+                using (XLWorkbook wb = new XLWorkbook(northwinddataXlsx)) { 
+                    var ws = wb.Worksheet(1);
 
-                var firstRowUsed = ws.FirstRowUsed();
-                var categoryRow = firstRowUsed.RowUsed();
-
-                categoryRow = categoryRow.RowBelow();
-
-                const int apartmentRow = 1;
-                const int modelRow = 2;
-                const int serialRow = 3;
-
-                while (!categoryRow.Cell(1).IsEmpty())
-                {
-                    string apartment = categoryRow.Cell(apartmentRow).GetString();
-                    string model = categoryRow.Cell(modelRow).GetString();
-                    string serial = categoryRow.Cell(serialRow).GetString();
-
-                    registersList.Add(new RegistryInfo(apartment, model, serial));
+                    var firstRowUsed = ws.FirstRowUsed();
+                    var categoryRow = firstRowUsed.RowUsed();
 
                     categoryRow = categoryRow.RowBelow();
-                }
 
+                    const int apartmentRow = 1;
+                    const int modelRow = 2;
+                    const int serialRow = 3;
+
+                    while (!categoryRow.Cell(1).IsEmpty())
+                    {
+                        string apartment = categoryRow.Cell(apartmentRow).GetString();
+                        string model = categoryRow.Cell(modelRow).GetString();
+                        string serial = categoryRow.Cell(serialRow).GetString();
+
+                        registersList.Add(new RegistryInfo(apartment, model, serial));
+
+                        categoryRow = categoryRow.RowBelow();
+                    }
+                }
                 Console.WriteLine();
                 return registersList;
             }
