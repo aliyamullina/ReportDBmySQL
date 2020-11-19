7add6bb4a65d1424272ad9e4dea895b9122f6ead
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-19 16:22:08 +0300

    Распарсить а, удаление примера - Ошибка

--- Classes/GetFillRegisters.cs
+++ Classes/GetFillRegisters.cs
@@ -18,32 +18,16 @@ namespace ReportDBmySQL
 
             List<InfoRegistry> registersListTable = new List<InfoRegistry>();
 
-            //c# return list in foreach loop???
-            //public IQueryable<StatusList> GetLocationStatus()
-            //{
-            //    var status = (from location in this.ObjectContext.Locations
-            //                  where location.InspectionReadings.Status == value
-            //                  orderby a.DateTaken
-            //                  select new LocationStatusList()
-            //                  {
-            //                      ID = a.ID,
-            //                      LocationName = d.Name,
-            //                  }).ToList<StatusList>();
-            //    return status;
-            //}
+            List<InfoRegistry> Register = (from InfoCatalog c in path
+                                           let a = GetExcelTableRead(c.Registry, out registersListTable)
+                                           select new InfoRegistry()
+                                           {
+                                               //Apartment = a,
+                                               //Model = m,
+                                               //Serial = s,
+                                           }).ToList<InfoRegistry>();;
 
-            //https://overcoder.net/q/1065266/%D0%BD%D0%B5-%D1%83%D0%B4%D0%B0%D0%B5%D1%82%D1%81%D1%8F-%D0%BD%D0%B5%D1%8F%D0%B2%D0%BD%D0%BE-%D0%BF%D1%80%D0%B5%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D1%82%D0%B8%D0%BF-systemcollectionsgenericlist-lttgt-%D0%B2
-
-            var Register = (from InfoCatalog c in path
-                           let a = GetExcelTableRead(c.Registry, out registersListTable)
-                           select new InfoRegistry()
-                           {
-                               Apartment = a,
-                               Model = m,
-                               Serial = s,
-                           }).ToList<InfoRegistry>();;
-
-            return (List<InfoRegistry>)Register;
+            return Register;
         }
 
         /// <summary>
