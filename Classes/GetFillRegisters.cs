c03c49473a197cef0a6163f2911effece6e4c340
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-19 12:25:55 +0300

    OfficeUtility в GetFillRegisters

--- Classes/GetFillRegisters.cs
+++ Classes/GetFillRegisters.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using ClosedXML.Excel;
+using System.Linq;
 
 namespace ReportDBmySQL
 {
@@ -10,17 +12,54 @@ namespace ReportDBmySQL
         /// </summary>
         public static List<InfoRegistry> GetFillRegisters()
         {
-            DB DBObject = new DB();
-            List<InfoCatalog> path = DBObject.GetCatalogList();
+            DB db = new DB();
+            List<InfoCatalog> path = db.GetCatalogList();
+
             List<InfoRegistry> registersListTable = new List<InfoRegistry>();
 
             foreach (InfoCatalog c in path)
             {
-                OfficeUtility.GetExcelTableRead(c.Registry, out registersListTable);
+                GetExcelTableRead(c.Registry, out registersListTable);
                 Console.WriteLine();
             }
             Console.WriteLine();
             return registersListTable;
+        }
+
+        /// <summary>
+        /// Читает данные из Excel
+        /// </summary>
+        /// <param name="northwinddataXlsx"></param>
+        /// <param name="registersList"></param>
+        /// <returns></returns>
+        public static List<InfoRegistry> GetExcelTableRead(string northwinddataXlsx, out List<InfoRegistry> registersList)
+        {
+            registersList = new List<InfoRegistry>();
+
+            try
+            {
+                using (XLWorkbook wb = new XLWorkbook(northwinddataXlsx))
+                {
+                    var ws = wb.Worksheet(1);
+
+                    var rows = ws.RangeUsed().RowsUsed().Skip(5);
+
+                    foreach (var row in rows)
+                    {
+
+                        string apartment = row.Cell(1).Value.ToString();
+                        string model = row.Cell(2).Value.ToString();
+                        string serial = row.Cell(3).Value.ToString();
+
+                        registersList.Add(new InfoRegistry(apartment, model, serial));
+                    }
+                }
+                return registersList;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
