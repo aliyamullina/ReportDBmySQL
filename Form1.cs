9856e057f7db2b57c67a4e9a869f4c88f787b735
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-20 11:56:48 +0300

    App test - OK

--- Form1.cs
+++ Form1.cs
@@ -12,23 +12,23 @@ namespace ReportDBmySQL
 
         private void Button1_Click(object sender, System.EventArgs e)
         {
-            //DB db = new DB();
+            DB db = new DB();
 
-            //db.CreateTableCatalogs();
-            //List<InfoCatalog> catalogsInsert = GetFill.GetFillCatalogs();
-            //db.InsertTableCatalogs(catalogsInsert);
+            db.CreateTableCatalogs();
+            List<InfoCatalog> catalogsInsert = GetFill.GetFillCatalogs();
+            db.InsertTableCatalogs(catalogsInsert);
 
-            //db.CreateTableCities();
-            //List<InfoCity> CitiesList = GetFill.GetFillCities();
-            //db.InsertTableCities(CitiesList);
+            db.CreateTableCities();
+            List<InfoCity> CitiesList = GetFill.GetFillCities();
+            db.InsertTableCities(CitiesList);
 
-            //db.CreateTableRegisters();
-            //List<InfoRegistry> RegistersList = GetFill.GetFillRegisters();
-            //db.InsertTableRegisters(RegistersList);
+            db.CreateTableRegisters();
+            List<InfoRegistry> RegistersList = GetFill.GetFillRegisters();
+            db.InsertTableRegisters(RegistersList);
 
-            //db.CreateTableAdresses();
-            //List<InfoAddress> addressesList = GetFill.GetFillAddresses();
-            //db.InsertTableAdresses(addressesList);
+            db.CreateTableAdresses();
+            List<InfoAddress> addressesList = GetFill.GetFillAddresses();
+            db.InsertTableAdresses(addressesList);
 
             GetCreateDoc.Create();
 
