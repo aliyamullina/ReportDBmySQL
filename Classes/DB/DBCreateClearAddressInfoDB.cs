039deff10da2d553abc1ea9a9a5289b14d51c6cc
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-25 16:06:07 +0300

    Классы DB, GetCreateDoc, GetFill - ОК

--- Classes/DB/DBCreateClearAddressInfoDB.cs
+++ Classes/DB/DBCreateClearAddressInfoDB.cs
@@ -6,45 +6,6 @@ namespace ReportDBmySQL
 {
     public partial class DB
     {
-        readonly MySqlConnection connection = new MySqlConnection(
-            "Server = localhost; " +
-            "Port = 3306; " +
-            "Username = root; " +
-            "Password = root; " +
-            "database = addressinfodb"
-            );
-
-        /// <summary>
-        /// Подключение к БД
-        /// </summary>
-        public void OpenConnection()
-        {
-            if (connection.State == System.Data.ConnectionState.Closed)
-            {
-                connection.Open();
-            }
-        }
-
-        /// <summary>
-        /// Отключение от БД
-        /// </summary>
-        public void CloseConnection()
-        {
-            if (connection.State == System.Data.ConnectionState.Open)
-            {
-                connection.Close();
-            }
-        }
-        
-        /// <summary>
-        /// Возвращает подключение к БД
-        /// </summary>
-        /// <returns></returns>
-        public MySqlConnection GetConnection()
-        {
-            return connection;
-        }
-
         /// <summary>
         /// Создается общая БД addressinfodb
         /// </summary>
