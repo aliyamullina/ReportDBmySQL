3ee476b636c422b0282757d6410a975dd7718323
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-25 15:35:57 +0300

    GetExcelTableRead и GetFillRegisters - ОК

--- Classes/GetFillRegisters.cs
+++ Classes/GetFillRegisters.cs
@@ -28,36 +28,5 @@ namespace ReportDBmySQL
                 return null; 
             }
         }
-
-        /// <summary>
-        /// Читает данные из Excel
-        /// </summary>
-        public static List<InfoRegistry> GetExcelTableRead(string pathRegistry, int catalog_id, out List<InfoRegistry> registersList)
-        {
-            registersList = new List<InfoRegistry>();
-
-            try
-            {
-                using (XLWorkbook wb = new XLWorkbook(pathRegistry))
-                {
-                    var ws = wb.Worksheet(1);
-                    var rows = ws.RangeUsed().RowsUsed().Skip(5);
-
-                    foreach (var row in rows)
-                    {
-                        string apartment = row.Cell(1).Value.ToString();
-                        string model = row.Cell(2).Value.ToString();
-                        string serial = row.Cell(3).Value.ToString();
-
-                        registersList.Add(new InfoRegistry(catalog_id, apartment, model, serial));
-                    }
-                }
-                return registersList;
-            }
-            catch
-            {
-                return null;
-            }
-        }
     }
 }
