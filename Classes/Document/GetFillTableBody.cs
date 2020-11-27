59b3c006cd7f89ea575bff40273a2985a5383173
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-27 10:34:40 +0300

    GetFillTableBody(): fileTable в tableBody, iT в tableRow - ОК

--- Classes/Document/GetFillTableBody.cs
+++ Classes/Document/GetFillTableBody.cs
@@ -11,22 +11,22 @@ namespace ReportDBmySQL
         /// </summary>
         private static void GetFillTableBody(Table table, string fN, MySqlConnection connection)
         {
-            List<InfoDocumentTable> fileTable = GetInfoDocumentTable(fN, connection);
+            List<InfoDocumentTable> tableBody = GetInfoDocumentTable(fN, connection);
 
             string comment = "В 2020 году истекает срок поверки. Требуется замена";
 
             int count = 1;
 
-            foreach (InfoDocumentTable iT in fileTable)
+            foreach (InfoDocumentTable tableRow in tableBody)
             {
                 TableRow bodyRow = new TableRow();
                 TableCell bodyTdCount = new TableCell(new Paragraph(new Run(new Text(count++.ToString()))));
-                TableCell bodyTdCity = new TableCell(new Paragraph(new Run(new Text(iT.City))));
-                TableCell bodyTdStreet = new TableCell(new Paragraph(new Run(new Text(iT.Street))));
-                TableCell bodyTdHome = new TableCell(new Paragraph(new Run(new Text(iT.Home))));
-                TableCell bodyTdApartment = new TableCell(new Paragraph(new Run(new Text(iT.Apartment))));
-                TableCell bodyTdModel = new TableCell(new Paragraph(new Run(new Text(iT.Model))));
-                TableCell bodyTdSerial = new TableCell(new Paragraph(new Run(new Text(iT.Serial))));
+                TableCell bodyTdCity = new TableCell(new Paragraph(new Run(new Text(tableRow.City))));
+                TableCell bodyTdStreet = new TableCell(new Paragraph(new Run(new Text(tableRow.Street))));
+                TableCell bodyTdHome = new TableCell(new Paragraph(new Run(new Text(tableRow.Home))));
+                TableCell bodyTdApartment = new TableCell(new Paragraph(new Run(new Text(tableRow.Apartment))));
+                TableCell bodyTdModel = new TableCell(new Paragraph(new Run(new Text(tableRow.Model))));
+                TableCell bodyTdSerial = new TableCell(new Paragraph(new Run(new Text(tableRow.Serial))));
                 TableCell bodyTdComment = new TableCell(new Paragraph(new Run(new Text(comment))));
                 bodyRow.Append(bodyTdCount, bodyTdCity, bodyTdStreet, bodyTdHome, bodyTdApartment, bodyTdModel, bodyTdSerial, bodyTdComment);
                 table.AppendChild(bodyRow);
