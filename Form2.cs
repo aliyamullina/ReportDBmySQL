80524cf8eb07326ba929c1c124030abfa7ab4fc7
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-30 15:00:57 +0300

    Form2: listbox, label, textbox - ОК

---
+++ Form2.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace ReportDBmySQL
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
