856d75275e1026759fa56457ac3403694f9c920d
Aliya Mullina <aliya.mullina@ya.ru>
2020-11-30 11:42:26 +0300

    Maps GetInsert() - ОК

---
+++ Classes/DatabaseTables/Maps/GetInsert.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace ReportDBmySQL
+{
+    public partial class Maps
+    {
+        /// <summary>
+        /// Заполнение таблицы Maps в БД
+        /// </summary>
+        public static void GetInsert(List<InfoMap> mapsList, MySqlConnection connection)
+        {
+            // Добавляет повторно, нет проверки на существование записи
+            using (MySqlCommand command = new MySqlCommand(@"
+                INSERT INTO maps(Floor, Apartment, Entrance) 
+                VALUES (@floor, @apartment, @entrance)
+                ", connection))
+            {
+                connection.Open();
+                foreach (var item in mapsList)
+                {
+                    command.Parameters.Clear();
+                    command.Parameters.AddWithValue("@floor", item.Floor);
+                    command.Parameters.AddWithValue("@apartment", item.Apartment);
+                    command.Parameters.AddWithValue("@entrance", item.Entrance);
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+    }
+}
