2abcf7b9e2f846942d46538ea5b25dd149a39c6a
Aliya Mullina <aliya.mullina@ya.ru>
2020-12-01 08:39:44 +0300

    Нет удаления, добавления в dataGridView1 - ОК

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -89,6 +89,8 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(6, 21);
             this.dataGridView1.Name = "dataGridView1";
