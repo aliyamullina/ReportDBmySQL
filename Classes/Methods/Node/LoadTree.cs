e8092979c15fbd8e75ba8d927de6120131c887d9
Aliya Mullina <aliya.mullina@ya.ru>
2020-12-01 15:26:24 +0300

    treeView1 добавляет адрес и данные с карты - ОК

---
+++ Classes/Methods/Node/LoadTree.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ReportDBmySQL
+{
+    public partial class Node
+    {
+        /// <summary>
+        /// Address
+        /// - Floor ​5 этажей
+        /// - FlatsCount 60
+        /// - Entrance В доме 4 подъезда
+        /// </summary>
+        public static void LoadTree(TreeView treeView1, List<InfoNode> nodeList)
+        {
+            treeView1.Nodes.Clear();
+
+            List<TreeNode> TestNodes = new List<TreeNode>();
+
+            foreach (InfoNode item in nodeList)
+            {
+                List<TreeNode> child = new List<TreeNode>
+                {
+                    new TreeNode(item.Floor),
+                    new TreeNode(item.FlatsCount),
+                    new TreeNode(item.Entrance)
+                };
+
+                TestNodes.Add(new TreeNode(item.Address, child.ToArray()));
+            }
+
+            treeView1.Nodes.AddRange(TestNodes.ToArray());
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -30,42 +30,12 @@ namespace ReportDBmySQL
             //Database.CreateTable(connection);
             //Database.GetFillTable(connection);
 
-            // Узел
             List<InfoNode> nodeList = Node.SelectAddresses(connection);
 
-            LoadTree(treeView1, nodeList);
+            Node.LoadTree(treeView1, nodeList);
 
             //Maps.GetFill();
 
-        }
-
-        public static void LoadTree(TreeView treeView1, List<InfoNode> nodeList)
-        {
-            // ​5 этажей, 60, В доме 4 подъезда
-            //Address
-            //    - Floor
-            //    - FlatsCount
-            //    - Entrance
-
-            treeView1.Nodes.Clear();
-
-            List<TreeNode> TestNodes = new List<TreeNode>();
-
-            foreach (InfoNode item in nodeList)
-            {
-                //List<TreeNode> parent = new List<TreeNode>();
-
-                List<TreeNode> child = new List<TreeNode>();
-
-                child.Add(new TreeNode(item.Floor, child.ToArray()));
-                child.Add(new TreeNode(item.FlatsCount, child.ToArray()));
-                child.Add(new TreeNode(item.Entrance, child.ToArray()));
-
-                TestNodes.Add(new TreeNode(item.Address, child.ToArray()));
-
-            }
-
-            treeView1.Nodes.AddRange(TestNodes.ToArray());
         }
 
         private void Button2_Click(object sender, EventArgs e)
