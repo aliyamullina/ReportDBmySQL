a783dc68a9574554294fcfb2ab08a8f547198d5d
Aliya Mullina <aliya.mullina@ya.ru>
2020-12-03 10:47:12 +0300

    Node GetInsert() передача id - Ошибки

--- Classes/Methods/Node/GetInsert.cs
+++ Classes/Methods/Node/GetInsert.cs
@@ -13,14 +13,23 @@ namespace ReportDBmySQL
             // Добавляет повторно, нет проверки на существование записи
             using (MySqlCommand command = new MySqlCommand(@"
             INSERT INTO maps(Floor, FlatsCount, Entrance) 
-            VALUES (@floor, @flatscount, @entrance)
+            VALUES (@floor, @flatscount, @entrance);
+            SELECT LAST_INSERT_ID();
+            SELECT 
+                    addresses.Address_id
+            FROM 
+                    addresses,
+                    cities
+            WHERE CONCAT(City, ', ',Street, ' ' ,Home) = @address
+            AND
+                VALUES (@floor, @flatscount, @entrance);
             ", connection))
             {
                 connection.Open();
                 foreach (var item in nodeListEdit)
                 {
                     command.Parameters.Clear();
-                    //command.Parameters.AddWithValue("@address", item.Address);
+                    command.Parameters.AddWithValue("@address", item.Address);
                     command.Parameters.AddWithValue("@floor", item.Floor);
                     command.Parameters.AddWithValue("@flatscount", item.FlatsCount);
                     command.Parameters.AddWithValue("@entrance", item.Entrance);
@@ -30,7 +39,13 @@ namespace ReportDBmySQL
             }
         }
     }
-
 }
+//INSERT INTO maps(Floor, FlatsCount, Entrance) VALUES('7 этажей', '89 квартир', '2 подъезда');
+//SELECT LAST_INSERT_ID();
+
+//INSERT INTO maps(Floor, FlatsCount, Entrance, Address_Id) 
+//VALUES('7 этажей', '89 квартир', '2 подъезда',
+//(SELECT addresses.Address_id FROM addresses, cities 
+//WHERE CONCAT(City, ', ', Street, ' ', Home) = 'Высокая гора, Большая Красная 214'));
 
 
