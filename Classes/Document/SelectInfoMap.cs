83ea2a762764f5e14995352bacc56ab23020a108
Aliya Mullina <aliya.mullina@ya.ru>
2020-12-03 15:09:56 +0300

    SelectInfoMap замена Null на 0 - ОК

--- Classes/Document/SelectInfoMap.cs
+++ Classes/Document/SelectInfoMap.cs
@@ -14,18 +14,11 @@ namespace ReportDBmySQL
 
             using (MySqlCommand command = new MySqlCommand(@"
                 SELECT 
-                    maps.Floor,
-                    maps.FlatsCount,
-                    maps.Entrance
-                FROM 
-                    addresses,
-                    cities,
-                    maps
-                WHERE CONCAT(City, ', ',Street, ' ' ,Home) = @address
-                AND
-                    addresses.Address_Id = maps.Address_Id
-                AND
-                    addresses.City_id = cities.City_Id
+                    maps.Address_id, 
+                    COALESCE(maps.Floor, 0), 
+                    COALESCE(maps.FlatsCount, 0), 
+                    COALESCE(maps.Entrance, 0)
+                FROM maps
                 ", connection))
             {
                 connection.Open();
@@ -54,3 +47,47 @@ namespace ReportDBmySQL
         }
     }
 }
+
+// https://stackoverflow.com/questions/59695041/how-do-i-coalesce-nulls-across-multiple-rows-in-bigquery
+
+//SELECT
+//    maps.Floor,
+//    maps.FlatsCount,
+//    maps.Entrance
+//FROM
+//    addresses,
+//    cities,
+//    maps
+//WHERE CONCAT(City, ', ', Street, ' ' , Home) = 'Высокая гора, Большая Красная 214'
+//AND
+//    addresses.Address_Id = maps.Address_Id
+//AND
+//    addresses.City_id = cities.City_Id
+
+//SELECT maps.Address_Id,
+//    Sum( Isnull(maps.Floor, 0) )      OVER(PARTITION BY maps.Address_id ORDER BY maps.Address_id),
+//    Sum( Isnull(maps.FlatsCount, 0) ) OVER(PARTITION BY maps.Address_id ORDER BY maps.Address_id),
+//    Sum( Isnull(maps.Entrance, 0) )   OVER(PARTITION BY maps.Address_id ORDER BY maps.Address_id)
+//FROM maps
+//GROUP BY maps.Address_id
+
+//SELECT id,
+//    Sum(Isnull(red, 0)) OVER(partition BY id ORDER BY id),        
+//    Sum(Isnull(buy, 0)) OVER(partition BY id  ORDER BY id),        
+//    Sum(Isnull(bsw, 0)) OVER(partition BY id  ORDER BY id)
+//FROM Table1GROUP  
+//BY id 
+
+//SELECT maps.Address_Id,
+//    Sum(COALESCE(maps.Floor, 0)) OVER(PARTITION BY maps.Address_id ORDER BY maps.Address_id),
+//    Sum(COALESCE(maps.FlatsCount, 0)) OVER(PARTITION BY maps.Address_id ORDER BY maps.Address_id),
+//    Sum(COALESCE(maps.Entrance, 0)) OVER(PARTITION BY maps.Address_id ORDER BY maps.Address_id)
+//FROM maps
+//GROUP BY maps.Address_id;
+
+//SELECT maps.Address_Id,
+//    Sum(COALESCE(red, 0)) OVER(partition BY maps.Address_Id ORDER BY maps.Address_Id),        
+//    Sum(COALESCE(buy, 0)) OVER(partition BY maps.Address_Id  ORDER BY maps.Address_Id),        
+//    Sum(COALESCE(bsw, 0)) OVER(partition BY maps.Address_Id  ORDER BY maps.Address_Id)
+//FROM Table1GROUP  
+//GROUP BY maps.Address_Id
