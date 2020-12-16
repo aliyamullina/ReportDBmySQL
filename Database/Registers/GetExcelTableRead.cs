45c6cf89a2d93d50bd148a16bffe5deb9d3b65ea
Aliya Mullina <aliya.mullina@ya.ru>
2020-12-16 16:47:26 +0300

    Database\Registers\GetExcelTableRead() упрощение

--- Database/Registers/GetExcelTableRead.cs
+++ Database/Registers/GetExcelTableRead.cs
@@ -9,31 +9,23 @@ namespace ReportDBmySQL
         /// <summary>
         /// Читает данные из Excel
         /// </summary>
-        public static List<InfoRegistry> GetExcelTableRead(string pathRegistry, int catalog_id, out List<InfoRegistry> registersList)
+        public static void GetExcelTableRead(string pathRegistry, int catalog_id, out List<InfoRegistry> catalogRegistersTable)
         {
-            registersList = new List<InfoRegistry>();
+            catalogRegistersTable = new List<InfoRegistry>();
 
-            try
+            using (XLWorkbook wb = new XLWorkbook(pathRegistry))
             {
-                using (XLWorkbook wb = new XLWorkbook(pathRegistry))
+                var ws = wb.Worksheet(1);
+                var rows = ws.RangeUsed().RowsUsed().Skip(5);
+
+                foreach (var row in rows)
                 {
-                    var ws = wb.Worksheet(1);
-                    var rows = ws.RangeUsed().RowsUsed().Skip(5);
+                    string apartment = row.Cell(1).Value.ToString();
+                    string model = row.Cell(2).Value.ToString();
+                    string serial = row.Cell(3).Value.ToString();
 
-                    foreach (var row in rows)
-                    {
-                        string apartment = row.Cell(1).Value.ToString();
-                        string model = row.Cell(2).Value.ToString();
-                        string serial = row.Cell(3).Value.ToString();
-
-                        registersList.Add(new InfoRegistry(catalog_id, apartment, model, serial));
-                    }
+                    catalogRegistersTable.Add(new InfoRegistry(catalog_id, apartment, model, serial));
                 }
-                return registersList;
-            }
-            catch
-            {
-                return null;
             }
         }
     }
