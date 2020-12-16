e770a0c7cfed23f66b8b3861c62233bce03e4159
Aliya Mullina <aliya.mullina@ya.ru>
2020-12-16 14:01:58 +0300

    Database\Registers\GetFill() registersTables не нужен

--- Database/Registers/GetFill.cs
+++ Database/Registers/GetFill.cs
@@ -8,21 +8,12 @@ namespace ReportDBmySQL
         /// <summary>
         /// Берет данные из массива и передает в коллекцию
         /// </summary>
-        public static List<InfoRegistry> GetFill(int catalog_id, List<InfoCatalog> path)
+        public static void GetFill(ref List<InfoRegistry> registersList, int catalog_id, List<InfoCatalog> path)
         {
-            try {
-                List<InfoRegistry> registersTables = new List<InfoRegistry>();
-
-                foreach (InfoCatalog c in path)
-                {
-                    GetExcelTableRead(c.Registry, catalog_id, out List <InfoRegistry> registersTable);
-                    registersTables= registersTable.Union(registersTables).ToList();
-                }
-                return registersTables;
-            }
-            catch 
-            { 
-                return null; 
+            foreach (InfoCatalog c in path)
+            {
+                GetExcelTableRead(c.Registry, catalog_id, out List <InfoRegistry> registersTable);
+                registersList= registersTable.Union(registersList).ToList();
             }
         }
     }
