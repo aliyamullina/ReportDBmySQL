1cd5ea988305823e137f619b405f9f57df79f0f7
Aliya Mullina <aliya.mullina@ya.ru>
2020-12-16 16:26:22 +0300

    List<InfoCatalog> path в  oneCatalogPath

--- Database/Registers/GetFillList.cs
+++ Database/Registers/GetFillList.cs
@@ -8,12 +8,12 @@ namespace ReportDBmySQL
         /// <summary>
         /// Берет данные из массива и передает в коллекцию
         /// </summary>
-        public static void GetFillList(ref List<InfoRegistry> registersList, int catalog_id, List<InfoCatalog> path)
+        public static void GetFillList(ref List<InfoRegistry> registersList, int catalog_id, List<InfoCatalog> oneCatalogPath)
         {
-            foreach (InfoCatalog c in path)
+            foreach (InfoCatalog c in oneCatalogPath)
             {
                 GetExcelTableRead(c.Registry, catalog_id, out List <InfoRegistry> registersTable);
-                registersList= registersTable.Union(registersList).ToList();
+                registersList = registersTable.Union(registersList).ToList();
             }
         }
     }
