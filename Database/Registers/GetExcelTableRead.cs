4cfcf533db6533c9ae7b6d7c256cf86ffbd2b81e
Aliya Mullina <aliya.mullina@ya.ru>
2020-12-26 15:46:06 +0300

    Ссылки, конвертация DateTime - Ошибки

--- Database/Registers/GetExcelTableRead.cs
+++ Database/Registers/GetExcelTableRead.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 
 namespace ReportDBmySQL
 {
@@ -10,10 +11,10 @@ namespace ReportDBmySQL
         /// <summary>
         /// Читает данные из Excel
         /// </summary>
-        public static void GetExcelTableRead(string pathRegistry, int catalog_id, out List<InfoRegistry> catalogRegistersTable, out List<DateTime> catalogDateTable)
+        public static void GetExcelTableRead(string pathRegistry, int catalog_id, out List<InfoRegistry> catalogRegistersTable, out List<DateTime> catalogDatesTable)
         {
             catalogRegistersTable = new List<InfoRegistry>();
-            catalogDateTable = new List<DateTime>();
+            catalogDatesTable = new List<DateTime>();
 
             using (XLWorkbook wb = new XLWorkbook(pathRegistry))
             {
@@ -27,10 +28,16 @@ namespace ReportDBmySQL
                     string serial = row.Cell(3).Value.ToString();
 
                     string date = row.Cell(10).Value.ToString();
+                    var dateFromString = DateTime.ParseExact(date, "yy/MM/dd", CultureInfo.InvariantCulture);
 
                     catalogRegistersTable.Add(new InfoRegistry(catalog_id, apartment, model, serial));
 
-                    catalogDateTable.Add(new DateTime(parsedDate));
+
+                    //catalogDatesTable.Add(new DateTime(dateFromString));
+
+                    //https://github.com/closedxml/closedxml/wiki
+                    //https://www.c-sharpcorner.com/UploadFile/mahesh/working-with-datetime-using-C-Sharp/
+                    //https://ru.stackoverflow.com/questions/916415/%D0%91%D0%BB%D0%B8%D0%B6%D0%B0%D0%B9%D1%88%D0%B5%D0%B5-%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D0%B5-listdatetime-%D1%87%D0%B5%D1%80%D0%B5%D0%B7-linq
                 }
             }
         }
