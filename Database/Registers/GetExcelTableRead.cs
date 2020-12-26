6f40418aa9fba65a04045392eb44d14bcce79152
Aliya Mullina <aliya.mullina@ya.ru>
2020-12-26 16:01:13 +0300

    date, date2 в формате DateTime - ОК

--- Database/Registers/GetExcelTableRead.cs
+++ Database/Registers/GetExcelTableRead.cs
@@ -27,17 +27,12 @@ namespace ReportDBmySQL
                     string model = row.Cell(2).Value.ToString();
                     string serial = row.Cell(3).Value.ToString();
 
-                    string date = row.Cell(10).Value.ToString();
-                    var dateFromString = DateTime.ParseExact(date, "yy/MM/dd", CultureInfo.InvariantCulture);
+                    var date = row.Cell(10).GetDateTime();
+                    var date2 = row.Cell(10).GetValue<DateTime>();
 
                     catalogRegistersTable.Add(new InfoRegistry(catalog_id, apartment, model, serial));
 
-
-                    //catalogDatesTable.Add(new DateTime(dateFromString));
-
-                    //https://github.com/closedxml/closedxml/wiki
-                    //https://www.c-sharpcorner.com/UploadFile/mahesh/working-with-datetime-using-C-Sharp/
-                    //https://ru.stackoverflow.com/questions/916415/%D0%91%D0%BB%D0%B8%D0%B6%D0%B0%D0%B9%D1%88%D0%B5%D0%B5-%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D0%B5-listdatetime-%D1%87%D0%B5%D1%80%D0%B5%D0%B7-linq
+                    catalogDatesTable.Add(new DateTime(date));
                 }
             }
         }
