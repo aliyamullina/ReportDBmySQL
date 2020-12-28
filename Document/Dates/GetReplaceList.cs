e75f464a1b01b16cbce365d88f1bbe78f07e6e63
Aliya Mullina <aliya.mullina@ya.ru>
2020-12-28 14:33:39 +0300

    Методы для Dates GetReplaceList() и GetSelectList()

---
+++ Document/Dates/GetReplaceList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ReportDBmySQL
+{
+    public partial class Dates
+    {
+        /// <summary>
+        /// Добавляет в doc данные по карте
+        /// </summary>
+        public static void GetReplaceList(in List<DateTime> documentDate, ref string docText)
+        {
+            foreach (DateTime item in documentDate)
+            {
+                docText = new Regex("DateInfo").Replace(docText, item.Date.ToString());
+            }
+        }
+    }
+}
---
+++ Document/Dates/GetSelectList.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace ReportDBmySQL
+{
+    public partial class Dates
+    {
+        /// <summary>
+        /// Возвращает реестр для текущего адреса в БД
+        /// </summary>
+        public static void GetSelectList(out List<DateTime> documentDate, in string fN, MySqlConnection connection)
+        {
+            documentDate = new List<DateTime>();
+
+            //"Казань, Ямашева 88"
+
+            using (MySqlCommand command = new MySqlCommand(@"
+                SELECT 
+                    dates.Date
+                FROM addresses, cities, dates 
+                WHERE CONCAT(City, ', ', Street, ' ' , Home) = @address
+                AND
+                 addresses.Address_Id = dates.Date_id
+                AND
+                 addresses.City_id = cities.City_Id
+                GROUP BY Address_id
+                ", connection))
+            {
+                connection.Open();
+                command.Parameters.Clear();
+                command.Parameters.AddWithValue("@address", fN);
+                command.ExecuteNonQuery();
+
+                using (MySqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        documentDate.Add((DateTime)dataReader["Date"]);
+                    }
+                    dataReader.Close();
+                }
+                connection.Close();
+            }
+        }
+    }
+}
