98d67a9cb1c08f390d62b285c6269af2415ceb4b
Aliya Mullina <aliya.mullina@ya.ru>
2021-01-19 11:05:57 +0300

    Получение данных из xml

--- Database/Maps/GetXMLFindList.cs
+++ Database/Maps/GetXMLFindList.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Xml;
 
 namespace ReportDBmySQL
 {
@@ -12,6 +13,27 @@ namespace ReportDBmySQL
             foreach(InfoMap i in nodeList)
             {
                 _ = i.Address;
+            
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.Load(@"C:\Users\User1_106\Desktop\Github\ReportDBmySQL\Database\Maps\maps.xml");
+
+                // получим корневой элемент
+                XmlElement xRoot = xDoc.DocumentElement;
+
+                //обход всех узлов в корневом элементе
+                foreach (XmlNode xNode in xRoot)
+                {
+                    //получаем атрибут name
+                    if (xNode.Attributes.Count > 0) { XmlNode attr = xNode.Attributes.GetNamedItem("name"); }
+                    
+                    foreach(XmlNode childnode in xNode.ChildNodes)
+                    {
+                        if (childnode.Name== "floor") { }
+                        if (childnode.Name== "flatscount") { }
+                        if (childnode.Name== "entrance") { }
+                    }
+
+                }
             }
         }
     }
