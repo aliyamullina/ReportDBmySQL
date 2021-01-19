1d3c9325c8e120d77a9843dd5d47d31719f6c4ac
Aliya Mullina <aliya.mullina@ya.ru>
2021-01-19 12:09:33 +0300

    Заменяет в nodeList данные из xDoc

--- Database/Maps/GetXMLFindList.cs
+++ Database/Maps/GetXMLFindList.cs
@@ -14,23 +14,42 @@ namespace ReportDBmySQL
             xDoc.Load(@"C:\Users\User1_106\Desktop\Github\ReportDBmySQL\Database\Maps\maps.xml");
             XmlElement xRoot = xDoc.DocumentElement;
 
+            string xmlEntrance = "";
+            string xmlFlatsCount = "";
+            string xmlFloor = "";
+
             foreach (InfoMap i in nodeList)
             {        
                 foreach (XmlNode xNode in xRoot)
                 {
-                    XmlNode attr = xNode.Attributes.GetNamedItem("name");
-
-                    if(i.Address == attr.Value)
+                    if (xNode.Attributes.Count > 0)
                     {
-                        foreach (XmlNode childnode in xNode.ChildNodes)
-                        {
-                            if (childnode.Name == "floor") { _ = childnode.InnerText; }
-                            if (childnode.Name == "flatscount") { _ = childnode.InnerText; }
-                            if (childnode.Name == "entrance") { _ = childnode.InnerText; }
+                        XmlNode attr = xNode.Attributes.GetNamedItem("name");
 
-                            _ = i.Entrance;
-                            _ = i.FlatsCount;
-                            _ = i.Floor;
+                        if (i.Address == attr.Value)
+                        {
+                            foreach (XmlNode childnode in xNode.ChildNodes)
+                            {
+                                if (childnode.Name == "floor")
+                                {
+                                    xmlFloor = childnode.InnerText;
+                                    nodeList.ForEach(x => x.Floor = x.Floor.Replace(i.Floor, xmlFloor));
+                                }
+
+                                if (childnode.Name == "flatscount")
+                                {
+                                    xmlFlatsCount = childnode.InnerText;
+                                    nodeList.ForEach(x => x.FlatsCount = x.FlatsCount.Replace(i.FlatsCount, xmlFlatsCount));
+                                }
+
+                                if (childnode.Name == "entrance")
+                                {
+                                    xmlEntrance = childnode.InnerText;
+                                    nodeList.ForEach(x => x.Entrance = x.Entrance.Replace(i.Entrance, xmlEntrance));
+                                }
+
+                                System.Console.WriteLine();
+                            }
                         }
                     }
                 }
