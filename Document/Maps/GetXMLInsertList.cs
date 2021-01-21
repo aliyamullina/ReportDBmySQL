de08f583ae1c865836720c18053d279e49af6a80
Aliya Mullina <aliya.mullina@ya.ru>
2021-01-21 11:47:12 +0300

    Создание узла xml address и его элементов

--- Document/Maps/GetXMLInsertList.cs
+++ Document/Maps/GetXMLInsertList.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Xml;
 
 namespace ReportDBmySQL
 {
@@ -9,7 +10,26 @@ namespace ReportDBmySQL
         /// </summary>
         public static void GetXMLInsertList(ref List<InfoMap> mapListInsert)
         {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(@"C:\Users\User1_106\Desktop\Github\ReportDBmySQL\Database\Maps\maps.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
 
+            foreach (InfoMap i in mapListInsert)
+            {
+                XmlElement addressElement = xDoc.CreateElement("address");
+                XmlAttribute nameAttribute = xDoc.CreateAttribute("name");
+
+                XmlElement floorElement = xDoc.CreateElement("floor");
+                XmlElement flatscounElement = xDoc.CreateElement("flatscount");
+                XmlElement entranceElemement = xDoc.CreateElement("entrance");
+
+                XmlText nameText = xDoc.CreateTextNode(i.Address);
+
+                XmlText floorText = xDoc.CreateTextNode(i.Floor);
+                XmlText flatscounText = xDoc.CreateTextNode(i.FlatsCount);
+                XmlText entranceText = xDoc.CreateTextNode(i.Entrance);
+
+            }
         }
     }
 }
